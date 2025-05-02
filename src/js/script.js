a480f432cb71af593bdc14194fa977ea058fd8ea
teddybearcafe <hiro@h2office.jp>
2025-05-03 08:47:24 +0900

    contactフォーム完成

--- src/js/script.js
+++ src/js/script.js
@@ -93,3 +93,41 @@ box.each(function(){
         }
    });
 });
+
+// タブ切り替え
+//任意のタブにURLからリンクするための設定
+function GethashID (hashIDName){
+	if(hashIDName){
+		//タブ設定
+		$('.tab-area li').find('a').each(function() { //タブ内のaタグ全てを取得
+			var idName = $(this).attr('href'); //タブ内のaタグのリンク名（例）#lunchの値を取得	
+			if(idName == hashIDName){ //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
+				var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
+				$('.tab-area li').removeClass("tab-button--active"); //タブ内のliについているactiveクラスを取り除き
+				$('.tab-area li a img').removeClass("tab-button__link-icon--active"); //タブ内のimgについているactiveクラスを取り除き
+				$(parentElm).addClass("tab-button--active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
+				$(parentElm).find('a img').addClass("tab-button__link-icon--active"); //対応するimgにactiveクラスを追加
+				//表示させるエリア設定
+				$(".tab-panel").removeClass("tab-panel--active"); //もともとついているis-activeクラスを取り除き
+				$(hashIDName).addClass("tab-panel--active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加	
+			}
+		});
+	}
+}
+
+//タブをクリックしたら
+$('.tab-button a').on('click', function() {
+	var idName = $(this).attr('href'); //タブ内のリンク名を取得	
+	GethashID (idName);//設定したタブの読み込みと
+	return false;//aタグを無効にする
+});
+
+
+// 上記の動きをページが読み込まれたらすぐに動かす
+$(window).on('load', function () {
+    $('.tab-area li:first-of-type').addClass("tab-button--active"); //最初のliにactiveクラスを追加
+    $('.tab-area li:first-of-type a img').addClass("tab-button__link-icon--active"); //最初のimgにactiveクラスを追加
+    $('.tab-panel:first-of-type').addClass("tab-panel--active"); //最初の.areaにis-activeクラスを追加
+	var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
+	GethashID (hashName);//設定したタブの読み込み
+});
