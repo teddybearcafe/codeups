912510585ff6a0c007418fb49aaddd91efc44ef0
teddybearcafe <hiro@h2office.jp>
2025-05-19 23:52:08 +0900

    ひととおり全て制作、あとはQC

--- src/js/micromodal.js
+++ src/js/micromodal.js
@@ -1,6 +1,28 @@
-
 // MicroModal初期化
-MicroModal.init({
-    awaitOpenAnimation: true,
-    awaitCloseAnimation: true
+const initModal = () => {
+  if (window.matchMedia("(min-width: 768px)").matches) {
+    MicroModal.init({
+      openTrigger: "data-micromodal-trigger",
+      closeTrigger: "data-micromodal-close",
+      openClass: "is-open",
+      disableScroll: true,
+      disableFocus: false,
+      awaitOpenAnimation: false,
+      awaitCloseAnimation: false,
+      onShow: (modal) => {
+        document.body.classList.add("is-modal-open");
+      },
+      onClose: (modal) => {
+        document.body.classList.remove("is-modal-open");
+      },
+    });
+  }
+};
+
+// 初期化
+initModal();
+
+// リサイズ時に再初期化
+window.addEventListener("resize", () => {
+  initModal();
 });
