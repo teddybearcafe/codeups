dc64ba34025fc6902d418f6996a56d927dee4eb6
teddybearcafe <hiro@h2office.jp>
2025-06-30 23:30:50 +0900

    URLに応じたtab開閉対応済み20250630

--- src/js/script.js
+++ src/js/script.js
@@ -102,48 +102,51 @@ box.each(function () {
 });
 
 //--------------------------------
-// タブ切り替え
+// information タブ切り替え
 //任意のタブにURLからリンクするための設定
-function GethashID(hashIDName) {
-  if (hashIDName) {
-    //タブ設定
-    $(".tab-area li")
-      .find("a")
-      .each(function () {
-        //タブ内のaタグ全てを取得
-        var idName = $(this).attr("href"); //タブ内のaタグのリンク名（例）#lunchの値を取得
-        if (idName == hashIDName) {
-          //リンク元の指定されたURLのハッシュタグ（例）http://example.com/#lunch←この#の値とタブ内のリンク名（例）#lunchが同じかをチェック
-          var parentElm = $(this).parent(); //タブ内のaタグの親要素（li）を取得
-          $(".tab-area li").removeClass("tab-button--active"); //タブ内のliについているactiveクラスを取り除き
-          $(".tab-area li a img").removeClass("tab-button__link-icon--active"); //タブ内のimgについているactiveクラスを取り除き
-          $(parentElm).addClass("tab-button--active"); //リンク元の指定されたURLのハッシュタグとタブ内のリンク名が同じであれば、liにactiveクラスを追加
-          $(parentElm).find("a img").addClass("tab-button__link-icon--active"); //対応するimgにactiveクラスを追加
-          //表示させるエリア設定
-          $(".tab-panel").removeClass("tab-panel--active"); //もともとついているis-activeクラスを取り除き
-          $(hashIDName).addClass("tab-panel--active"); //表示させたいエリアのタブリンク名をクリックしたら、表示エリアにis-activeクラスを追加
-        }
-      });
+//---------------------------------
+$(function () {
+  // パラメータ取得
+  function getParam(name, url) {
+    if (!url) url = window.location.href;
+    name = name.replace(/[\[\]]/g, "\\$&");
+    let regex = new RegExp("[?&]" + name + "(=([^&#]*)|&|#|$)"),
+      results = regex.exec(url);
+    if (!results) return null;
+    if (!results[2]) return "";
+    return decodeURIComponent(results[2].replace(/\+/g, " "));
   }
-}
 
-//タブをクリックしたら
-$(".tab-button a").on("click", function () {
-  var idName = $(this).attr("href"); //タブ内のリンク名を取得
-  GethashID(idName); //設定したタブの読み込みと
-  return false; //aタグを無効にする
+  // ページ読み込み時のタブ切り替え
+  let pram = getParam("active-tab");
+  if (pram && $('[data-tab="' + pram + '"]').length) {
+    $(".tab-button").removeClass("tab-button--active");
+    $(".tab-panel").removeClass("tab-panel--active");
+    $(".tab-button__link-icon").removeClass("tab-button__link-icon--active");
+
+    $('.tab-button[data-tab="' + pram + '"]').addClass("tab-button--active");
+    $('.tab-button[data-tab="' + pram + '"] .tab-button__link-icon').addClass(
+      "tab-button__link-icon--active"
+    );
+    $('.tab-panel[data-tab="' + pram + '"]').addClass("tab-panel--active");
+  }
+
+  // ロード後のタブ切り替え
+  $(".tab-button").on("click", function () {
+    let dataTab = $(this).data("tab"); // data-tab属性の値を取得
+    $(".tab-button").removeClass("tab-button--active");
+    $(".tab-panel").removeClass("tab-panel--active");
+    $(".tab-button__link-icon").removeClass("tab-button__link-icon--active");
+
+    $(this).addClass("tab-button--active");
+    $(this)
+      .find(".tab-button__link-icon")
+      .addClass("tab-button__link-icon--active");
+    $('.tab-panel[data-tab="' + dataTab + '"]').addClass("tab-panel--active");
+  });
 });
 
-// 上記の動きをページが読み込まれたらすぐに動かす
-$(window).on("load", function () {
-  //$(".tab-area li:first-of-type").addClass("tab-button--active"); //最初のliにactiveクラスを追加
-  //$(".tab-area li:first-of-type a img").addClass(
-  //  "tab-button__link-icon--active"
-  //); //最初のimgにactiveクラスを追加
-  //$(".tab-panel:first-of-type").addClass("tab-panel--active"); //最初の.areaにis-activeクラスを追加
-  var hashName = location.hash; //リンク元の指定されたURLのハッシュタグを取得
-  GethashID(hashName); //設定したタブの読み込み
-});
+
 
 // 最初のアーカイブアイテムを開いた状態にする
 $(document).ready(function () {
